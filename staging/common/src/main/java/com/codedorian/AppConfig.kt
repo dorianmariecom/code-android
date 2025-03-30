096c84861c691e70bbfc2c05f63486b781130aaa
Dorian Marié <dorian@dorianmarie.com>
2025-03-30 11:19:04 +0200

    make it work

---
+++ staging/common/src/main/java/com/codedorian/AppConfig.kt
@@ -0,0 +1,18 @@
+package com.codedorian
+
+object AppConfig {
+    val environment: String = BuildConfig.CODE_ENV
+
+    val baseDomain: String = when (environment) {
+        "test" -> "http://localhost:3000"
+        "localhost" -> "http://localhost:3000"
+        "development" -> "https://dev.codedorian.com"
+        "staging" -> "https://staging.codedorian.com"
+        else -> "https://codedorian.com"
+    }
+
+    val baseURL: String = baseDomain
+    val configurationsURL: String = "$baseDomain/configurations/android_v1.json"
+    val devicesURL: String = "$baseDomain/devices"
+    var csrfToken: String? = null
+}
