de39bd4ce244d293f7a294deac71fa876dd204c6
Dorian Marié <dorian@dorianmarie.com>
2025-03-30 10:33:42 +0200

    change to regular files instead of symlinks

--- localhost/src/main/java/com/codedorian/MainActivity.kt
+++ localhost/src/main/java/com/codedorian/MainActivity.kt
@@ -1,1 +1,35 @@
-production/src/main/java/com/codedorian/MainActivity.kt
+package com.codedorian
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import dev.hotwire.core.config.Hotwire
+import dev.hotwire.core.turbo.config.PathConfiguration
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
+import dev.hotwire.navigation.util.applyDefaultImeWindowInsets
+
+class MainActivity : HotwireActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Toast.makeText(this, BuildConfig.CODE_ENV ?: "unknown", Toast.LENGTH_SHORT).show()
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.main_nav_host).applyDefaultImeWindowInsets()
+        Hotwire.loadPathConfiguration(
+            context = this,
+            location = PathConfiguration.Location(
+                remoteFileUrl = AppConfig.configurationsURL
+            )
+        )
+    }
+
+    override fun navigatorConfigurations() = listOf(
+        NavigatorConfiguration(
+            name = "main",
+            startLocation = AppConfig.baseURL,
+            navigatorHostId = R.id.main_nav_host
+        )
+    )
+}
