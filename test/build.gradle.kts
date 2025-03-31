a7ff3f7295d0cb3c5c352685eadd81b22b09a8a2
Dorian Marié <dorian@dorianmarie.com>
2025-03-31 14:51:58 +0200

    theme

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -4,42 +4,65 @@ plugins {
 }
 
 android {
-    namespace = "com.codedorian.test"
+    namespace = "com.codedorian"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.codedorian.test"
+        applicationId = "com.codedorian"
         minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    buildFeatures {
+        buildConfig = true
+    }
+
+    signingConfigs {
+        delete("debug")
+
+        delete("release")
+
+        create("ttest") {
+            storeFile = file("keystores/test.keystore")
+            storePassword = "testtest"
+            keyAlias = "test"
+            keyPassword = "testtest"
+        }
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        delete("debug")
+
+        delete("release")
+
+        create("ttest") {
+            applicationIdSuffix = ".ttest"
+            versionNameSuffix = "-test"
+            buildConfigField("String", "CODE_ENV", "\"test\"")
+            signingConfig = signingConfigs.getByName("ttest")
+            resValue("string", "app_name", "test")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.hotwire.core)
+    implementation(libs.hotwire.navigation.fragments)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
