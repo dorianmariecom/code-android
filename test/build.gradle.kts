ae82ba2a39d2ab3b95ea3d777123b412bc223bc9
Dorian Marié <dorian@dorianmarie.com>
2025-04-05 17:15:00 +0200

    bump

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.codedorian"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.6"
     }
 
     buildFeatures {
