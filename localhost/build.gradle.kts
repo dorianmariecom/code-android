ae0a13dc8789f727fcb87365e15335160768bae9
Dorian Marié <dorian@dorianmarie.com>
2025-05-06 20:14:59 +0200

    v1.8/build 8 with notification on android 11

--- localhost/build.gradle.kts
+++ localhost/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.codedorian"
         minSdk = 28
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
     }
 
     buildFeatures {
