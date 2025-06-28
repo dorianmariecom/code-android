42947d00c951f62f2cb5d115d79058903771e74c
Dorian Marié <dorian@dorianmarie.com>
2025-06-28 15:41:04 +0200

    bump to 2.1 / 201

--- staging/build.gradle.kts
+++ staging/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.codedorian"
         minSdk = 28
         targetSdk = 35
-        versionCode = 200
-        versionName = "2.0"
+        versionCode = 201
+        versionName = "2.1"
     }
 
     buildFeatures {
